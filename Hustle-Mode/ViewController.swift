6561165f5e7d2cf6a2007e65a5936de96ac3fb1f
Noi <starrpatty28@gmail.com>
2019-03-13 06:46:27 -0700

    Messages
    Added messages to each chunk of code to know what was done.

--- Hustle-Mode/ViewController.swift
+++ Hustle-Mode/ViewController.swift
@@ -11,6 +11,7 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    // This is making a connection between interface and code
     @IBOutlet var darkBlueBG: UIImageView!
     @IBOutlet var powerBtn: UIButton!
     @IBOutlet var cloudHolder: UIView!
@@ -18,11 +19,13 @@ class ViewController: UIViewController {
     @IBOutlet var hustleLBL: UILabel!
     @IBOutlet var onLBL: UILabel!
     
+    // Create a Player that will actually play music
     var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Went and got the Path for the wavw/sound file and loaded it into the player
         let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
         let url = URL(fileURLWithPath: path)
         do  {
@@ -33,14 +36,17 @@ class ViewController: UIViewController {
         }
     }
     
-    
+    // When the Pwr Btn is pressed its hidden w/bckgrnd but cloudholder shows
     @IBAction func powerBtnPressed(_ sender: Any) {
         cloudHolder.isHidden = false
         darkBlueBG.isHidden = true
         powerBtn.isHidden = true
         
+        // Play the sound effects
         player.play()
         
+        
+        // While the music is playing move the rocket up the screen for 2.3 seconds
         UIView.animate(withDuration: 2.3, animations: {
             self.rocket.frame = CGRect(x: 0, y: 20, width: 375, height: 151)
             
