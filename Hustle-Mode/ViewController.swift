5431a7e0a28cb1b6a713fbc865e0fb9db6442350
Noi <starrpatty28@gmail.com>
2019-03-13 03:58:37 -0700

    IB Outlets
    Add 3 IB Outlets for buttons, views, clouds

--- Hustle-Mode/ViewController.swift
+++ Hustle-Mode/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var darkBlueBG: UIImageView!
+    @IBOutlet var powerBtn: UIButton!
+    @IBOutlet var cloudHolder: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
